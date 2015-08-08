a3acb8a07362837319744fbd5e42cf45d90ca3ab
JenittaMary <beaulahjeni@gmail.com>
2015-08-08 12:19:00 +0530

    Modified

--- HelloJava.java
+++ HelloJava.java
@@ -1,10 +1,11 @@
-
+import java.io.*;
 public class HelloJava {
 	 public static void main(String args[])
 	{
 		 System.out.println("hai");
 	System.out.println("Hello Java ");
-	
+
+	System.out.println("Here I added a new line");
 	}
 	} 
 
